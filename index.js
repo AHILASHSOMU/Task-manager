1f0b0a75517a5381810e41964e6fb7b61128e002
AHILASHSOMU <115772511+AHILASHSOMU@users.noreply.github.com>
2023-05-04 14:21:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ app.use(express.json({ limit: '100mb' }))
 app.use(express.urlencoded({ extended: true }));
 
 app.use( cors())
+app.get("/", (req, res) => {
+    res.setHeader("Access-Control-Allow-Credentials","true");
+    res.send("API is running..");
+});
+
 app.use("/user", userRoute);
 
 db.sequelize.sync().then(()=>{
